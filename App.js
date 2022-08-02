23bbfb60589bd43f956aaf1f00179425c1dc02dc
Aluno15 <luciofreitas059@gmail.com>
2022-08-02 16:00:02 -0400

    commit final

---
+++ App.js
@@ -0,0 +1,44 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from "@react-navigation/stack";
+import Details from "./src/pages/Details/index"
+import Task from "./src/pages/Task/index";
+import NewTask from "./src/pages/NewTask/index"
+
+
+
+const Stack = createStackNavigator();
+
+export default function App() {
+
+  return (
+   <NavigationContainer>
+    <Stack.Navigator initialRouteName="Task">
+      <Stack.Screen
+      name="Task"
+      component={Task}
+      options={{
+        headerTintColor:"#f92e6a"
+      }}
+      />
+      <Stack.Screen
+      name="NewTask"
+      component={NewTask}
+      options={{
+        headerTintColor:"#f92e6a"
+      }}
+      />
+      <Stack.Screen
+      name="Details"
+      component={Details}
+      options={{
+        headerTintColor:"#f92e6a"
+      }}
+      />
+    </Stack.Navigator>
+   </NavigationContainer>
+  );
+}
+
+
